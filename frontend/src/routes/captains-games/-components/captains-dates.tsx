77bf87d35f14e772dfc6ef1aaa2e1fb9b07df89a
Gabriel Lee <chihonglee777@gmail.com>
2025-06-09 23:09:07 +0100

    feat: more date charts

--- frontend/src/routes/captains-games/-components/captains-dates.tsx
+++ frontend/src/routes/captains-games/-components/captains-dates.tsx
@@ -10,7 +10,7 @@ dayjs.extend(duration)
 
 const data = rawData as CaptainMatch[];
 
-const dates = data.reduce((prev, match) => {
+const gameDates = data.reduce((prev, match) => {
   const startDate = dayjs.utc(match['Ending date']).subtract(dayjs.duration(match['Game length'].replaceAll(' ', '').toUpperCase())).format('YYYY-MM-DD');
   if (!prev[startDate]) {
     prev[startDate] = 0;
@@ -19,24 +19,58 @@ const dates = data.reduce((prev, match) => {
   return prev;
 }, {} as Record<string, number>);
 
-const chartData = Object.entries(dates).map(([day, value]) => ({day, value}));
+const gameDatesChartData = Object.entries(gameDates).map(([day, value]) => ({day, value}));
+
+const playerDates = data.reduce((prev, match) => {
+  const startDate = dayjs.utc(match['Ending date']).subtract(dayjs.duration(match['Game length'].replaceAll(' ', '').toUpperCase())).format('YYYY-MM-DD');
+  if (!prev[startDate]) {
+    prev[startDate] = new Set();
+  }
+  [match['Captain North'], match['Captain South'], ...match['North picks'], ...match['South picks']].filter((i) => i).forEach((player) => {
+    prev[startDate].add(player as string);
+  });
+  return prev;
+}, {} as Record<string, Set<string>>);
+
+const playerDatesChartData = Object.entries(playerDates).map(([day, value]) => ({day, value: value.size}));
 
 export const CaptainsDates = () => (
-  <div
-    style={{
-      height: "500px",
-    }}
-  >
-    <ResponsiveCalendar
-      data={chartData}
-      from={chartData.at(-1)?.day || ""} to={chartData[0]?.day || ""}
-      emptyColor="#eeeeee"
-      margin={{ top: 30, right: 10, bottom: 10, left: 20 }}
-      align="left"
-      yearSpacing={40}
-      monthBorderColor="#ffffff"
-      dayBorderWidth={2}
-      dayBorderColor="#ffffff"
-    />
+  <div>
+    <span># of games played per day</span>
+    <div
+      style={{
+        height: "500px",
+      }}
+    >
+      <ResponsiveCalendar
+        data={gameDatesChartData}
+        from={gameDatesChartData.at(-1)?.day || ""} to={gameDatesChartData[0]?.day || ""}
+        emptyColor="#eeeeee"
+        margin={{ top: 30, right: 10, bottom: 10, left: 20 }}
+        align="left"
+        yearSpacing={40}
+        monthBorderColor="#ffffff"
+        dayBorderWidth={2}
+        dayBorderColor="#ffffff"
+      />
+    </div>
+    <span># of unique players per day</span>
+    <div
+      style={{
+        height: "500px",
+      }}
+    >
+      <ResponsiveCalendar
+        data={playerDatesChartData}
+        from={playerDatesChartData.at(-1)?.day || ""} to={playerDatesChartData[0]?.day || ""}
+        emptyColor="#eeeeee"
+        margin={{ top: 30, right: 10, bottom: 10, left: 20 }}
+        align="left"
+        yearSpacing={40}
+        monthBorderColor="#ffffff"
+        dayBorderWidth={2}
+        dayBorderColor="#ffffff"
+      />
+    </div>
   </div>
 );
